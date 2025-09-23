3086a67bc18ce8fcbdad6c86f50621d05c042e0d
Bartosz-Júnior <edvaldo.sjr94@gmail.com>
2025-09-23 19:26:38 -0300

    Menu criado com as funcionalidades 1,2 5,6 e 0 funcionando. ainda falta tratar a apresentação dos dados(formatação).

--- cad_eventos.py
+++ cad_eventos.py
@@ -87,38 +87,50 @@ class Evento:
 #mini_curso.status()
 
 class Workshop(Evento):
-    def __init__(self, nome, data_evento, local, capacidade_max, categoria, preco_ingresso):
+    def __init__(self, nome = None, data_evento= None, local = None, capacidade_max = None, categoria = None, preco_ingresso = None):
         super().__init__(nome, data_evento, local, capacidade_max, categoria, preco_ingresso)
-        self.workshop = []
 
-    def add_evento(self, Workshop):
-        self.workshop.append(Workshop)
-
-    def listar_eventos(self):
-        for workshop in self.workshop:
-            print(workshop)
-
-class Palestra(Evento):
-    def __init__(self, nome, data_evento, local, capacidade_max, categoria, preco_ingresso):
-        super().__init__(nome, data_evento, local, capacidade_max, categoria, preco_ingresso)
-        self.palestra = []
-
-    def add_evento(self):
-        nome = str(input("Nome: "))
-        data_evento = str(input("Data: "))
+    def add_workshop(self):
+        nome = str(input("Tema do Workshop: "))
+        data_evento = str(input("Data de realização: "))
         local_evento = str(input("Local: "))
-        capacidade_max = int(input("Número máximo de pessoas: "))
+        capacidade_max = int(input("Capacidade de pessoas: "))
         categoria = str(input("Categoria [Tech/Marketing]: "))
-        preco_ingresso = float(input("Preço da entrada: "))
-        dados = {"Palestra: " : nome, "Data: " : data_evento, "Local " : local_evento, "Capacidade: " : capacidade_max,
+        preco_ingresso = float(input("Preço da entrada: R$ "))
+        dados_workshop = {"Palestra: " : nome, "Data: " : data_evento, "Local " : local_evento, "Capacidade: " : capacidade_max,
                  "Categoria: " : categoria, "Preço ingresso: " : preco_ingresso}
-        file = open("palestras.txt", "a")
-        file.write(str(dados))
+        file = open("workshop.txt", "a")
+        file.write(str(dados_workshop) + "\n")
         file.close()
 
-    def listar_eventos(self):
+    def listar_workshop(self):
+        arquivo = open("C:/Users/Júnior/Documents/Projeto_02_BFD/PROJETO_02-GEST-O_DE_EVENTOS/workshop.txt", "r")
+        conteudo = arquivo.readlines()
+        for dados in conteudo:
+            print(dados)
+            print("\n")
+
+class Palestra(Evento):
+    def __init__(self, nome = None, data_evento= None, local = None, capacidade_max = None, categoria = None, preco_ingresso = None):
+        super().__init__(nome, data_evento, local, capacidade_max, categoria, preco_ingresso)
+
+    def add_palestra(self):
+        nome = str(input("Tema da palestra: "))
+        data_evento = str(input("Data de realização: "))
+        local_evento = str(input("Local: "))
+        capacidade_max = int(input("Capacidade de pessoas: "))
+        categoria = str(input("Categoria [Tech/Marketing]: "))
+        preco_ingresso = float(input("Preço da entrada: "))
+        dados_palestra = {"Palestra: " : nome, "Data: " : data_evento, "Local " : local_evento, "Capacidade: " : capacidade_max,
+                 "Categoria: " : categoria, "Preço ingresso: " : preco_ingresso}
+        file = open("palestras.txt", "a")
+        file.write(str(dados_palestra) + "\n")
+        file.close()
+
+    def listar_palestras(self):
         arquivo = open("C:/Users/Júnior/Documents/Projeto_02_BFD/PROJETO_02-GEST-O_DE_EVENTOS/palestras.txt", "r")
         conteudo = arquivo.readlines()
         for dados in conteudo:
             print(dados)
+            print("\n")
     
--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 import cad_eventos as evento
 
+while True:
+    try:
+        print("*" * 35)
+        print("[1] Adicionar PALESTRA")
+        print("[2] Adicionar WORKSHOP")
+        print("[3] Fazer inscrição")
+        print("[4] Cancelar inscrição")
+        print("[5] Listar Palestras")
+        print("[6] Listar Workshops")
+        print("[7] Listar todos os eventos")
+        print("[0] Sair")
+        print('*' * 35)
 
-#minha_palestra = evento.Palestra("Python", "25/09/2025", "Recife", 150, "Tech", 35)
-#print(minha_palestra.status())
-#minha_palestra.add_evento()
-#minha_palestra.listar_eventos()
+        # Entrada do usuário para escolha da opção
+        escolha = int(input("Escolha uma opção: "))
 
-palestra_2 = evento.Palestra("Python", "25/09/2025", "Recife", 150, "Tech", 35)
-#palestra_2.add_evento()
-palestra_2.listar_eventos()
+        match escolha:
+            case 1:
+                palestra = evento.Palestra()
+                palestra.add_palestra()
+            case 2:
+                workshop = evento.Workshop()
+                workshop.add_workshop()
+            case 3:
+                pass
+            case 4:
+                pass
+            case 5:
+                palestra.listar_palestras()
+            case 6:
+                workshop.listar_workshop()
+
+    except ValueError:
+        print("Erro! Digite um número inteiro, ou 0 para sair.\n")
+        continue
