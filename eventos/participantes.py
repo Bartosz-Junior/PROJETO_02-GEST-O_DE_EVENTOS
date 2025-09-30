2120a66d515dc3e4c46a4fad871eba5f8b34855e
Carl Henrique <carlhenriquedev@gmail.com>
2025-09-30 07:52:12 -0300

    refactor: encapsulamento da classe participante

--- eventos/participantes.py
+++ eventos/participantes.py
@@ -1,12 +1,20 @@
 class Participante:
     def __init__(self, nome, email, evento_escolhido):
-        self.nome = nome
-        self.email = email
-        self.evento = evento_escolhido
+        self._nome = nome
+        self._email = email
+        self._evento = evento_escolhido
 
+    @property
+    def nome(self):
+        return self._nome
 
-    def verificar_lotacao(self):
-        self.evento
+    @property
+    def email(self):
+        return self._email
 
-    def exibir_participante(self):
-        return f"{self.nome} <{self.email}> <{self.evento}>"
+    @property
+    def evento(self):
+        return self._evento
+
+    def __str__(self):
+        return f"{self._nome} <{self._email}> <{self._evento}>"
