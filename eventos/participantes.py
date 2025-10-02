7fb7c5d6eca5d9b69419f313f5890f1d28026157
Carl Henrique <carlhenriquedev@gmail.com>
2025-10-02 10:01:17 -0300

    feat: metodo para salvar participante no json

--- eventos/participantes.py
+++ eventos/participantes.py
@@ -1,3 +1,8 @@
+from eventos.palestra import Palestra
+from eventos.workshop import Workshop
+import json
+import os
+
 class Participante:
     def __init__(self, nome, email, evento_escolhido):
         self._nome = nome
@@ -15,6 +20,16 @@ class Participante:
     @property
     def evento(self):
         return self._evento
+    
+    def salvar_participante(self, dados_participante, evento_escolhido):
+        with open("database/participantes.json", "r", encoding= "utf-8") as file:
+            carrega_participantes = json.load(file)
+            carrega_participantes.append(dados_participante)
+                    
+        with open("database/participantes.json", "w", encoding= "utf-8") as file:
+            json.dump(carrega_participantes, file, indent= 4, ensure_ascii= False)
+
+        evento_escolhido.adicionar_inscrito()
 
     def __str__(self):
         return f"{self._nome} <{self._email}> <{self._evento}>"
